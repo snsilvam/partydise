5c27556b31133b2342658afe17b91e9365ebac2d
unknown <silva.mendieta.nicolas@gmail.com>
2023-03-12 08:20:06 -0500

    Added the implementation var on repository

--- repository/OurServices/ourServices.repository.go
+++ repository/OurServices/ourServices.repository.go
@@ -6,6 +6,21 @@ import (
 	"github.com/snsilvam/partydise.git/models"
 )
 
-type OurServices interface {
+type OurServicesRepository interface {
 	InsertOurServices(ctx context.Context, service *models.OurServices) error
+	GetOurServicesById(ctx context.Context, id int64) (*models.OurServices, error)
+}
+
+var implementation OurServicesRepository
+
+func SetRepository(repository OurServicesRepository) {
+	implementation = repository
+}
+
+func InsertOurServices(ctx context.Context, service *models.OurServices) error {
+	return implementation.InsertOurServices(ctx, service)
+}
+
+func GetOurServicesById(ctx context.Context, id int64) (*models.OurServices, error) {
+	return implementation.GetOurServicesById(ctx, id)
 }
